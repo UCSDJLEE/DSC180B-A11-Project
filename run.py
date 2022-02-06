c458ff45fbce461b5c3f59b8fb5c48769bdff7cf
h8lee <h8lee@h8lee-32748.pods.h8lee.svc.cluster.local>
2022-02-05 21:07:56 -0800

    Demo run.py complete

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ sys.path.insert(0, 'src')
 
 from load_data import path_generator, load_jet_features, load_num_sv
 from mass_distribution import mass_distribution
-from sv_mass_distribution import sv_masss_distribution
+from sv_mass_distribution import sv_mass_distribution
 from jet_type_validation import jet_type_validation
 
 
@@ -24,14 +24,17 @@ def main(targets):
         df_qcd = load_jet_features(qcd_eda_sets)
         df_signal = load_jet_features(signal_eda_sets)
 
-        msg2 = f'In {len(qcd_eda_sets)} QCD files, there are {df_qcd.shape[0]} jet observations while in {len(signal_eda_sets)} Signal files, there are {df_signal.shape[0]} jets'
+        msg2 = f'In {len(qcd_eda_sets)} QCD files, there are {df_qcd.shape[0]} jet observations while in {len(signal_eda_sets)} Signal files, there are {df_signal.shape[0]} signal jets'
         print(msg2, '\n')
 
         df_signal_labels, signal_counts = jet_type_validation(df_signal, 'signal')
 
         msg3 = f'In {len(signal_eda_sets)} Signal files, different type of signal jets are distributed as:'
         print(msg3)
-        display(signal_counts)
+        with pd.option_context('display.max_rows', None,
+                              'display.max_columns', None,
+                              'display.precision', 3):
+            print(signal_counts)
         print('')
 
         signal_idx = df_signal_labels.index.tolist()
@@ -43,15 +46,19 @@ def main(targets):
         hist, summary = mass_distribution(df_qcd_and_signal)
 
         msg4 = 'Average and median jet mass for each type of jets'
-        print(msg4, '\n')
-        display(summary)
+        print(msg4)
+        with pd.option_context('display.max_rows', None,
+                              'display.max_columns', None,
+                              'display.precision', 3):
+            print(summary)
 
-        msg5 = 'With descriptive analysis and EDA covered, we\'re left to train our regressor model that estimates jet mass based on its properties'
-        print(msg5, '\n', 'run.py complete.')
+        msg5 = 'With descriptive analysis and EDA covered, we\'re left to train our regressor model that estimates jet mass based on its properties.'
+        print(msg5, '\n')
+        print('Demo run.py complete.')
 
     return
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     targets = sys.argv[1:]
     main(targets)
 
